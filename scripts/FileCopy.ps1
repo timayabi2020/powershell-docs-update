ed6a2e8f343a46621f9ecf40c81b504e0e82d414
Timothy Wamalwa <10947120+timayabi2020@users.noreply.github.com>
2022-08-04 22:46:00 +0300

    skipped one step

--- scripts/FileCopy.ps1
+++ scripts/FileCopy.ps1
@@ -94,5 +94,5 @@ if ($ModulesToGenerate.Count -eq 0) {
 Set-Location ..\powershell-docs-update
 Write-Host -ForegroundColor Green "-------------finished checking out to today's branch-------------"
 Start-Copy -ModulesToGenerate $ModulesToGenerate
-Write-Host -ForegroundColor Green "-------------Done-------------"
 git push -u origin
+Write-Host -ForegroundColor Green "-------------Done-------------"
